250c194b7ea3bd9afe9b65e453adf640cd2dc35f
Rashmi Panda <rrashmipanda2400@gmail.com>
2025-11-23 06:38:35 +0530

    patients api updated

---
+++ medilink/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import json
+
+
+app = FastAPI()
+
+
+
+def load_data():
+    with open('patients.json','r') as f:
+        data=json.load(f)
+
+    return data   
+
+
+@app.get("/")
+def hello():
+    return {"message":" patient management system "}
+
+
+@app.get("/about")
+def about():
+    return{"message":" A Fully functional API to manage your patient records."}
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
