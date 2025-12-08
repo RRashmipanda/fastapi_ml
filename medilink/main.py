e1f36f772927a090fe515e7d6cff2a2971e24b6a
Rashmi Panda <rrashmipanda2400@gmail.com>
2025-12-08 19:28:28 +0530

    basemodel updated

--- medilink/main.py
+++ medilink/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI,Path,HTTPException,Query
+from pydantic import BaseModel, Field
+from typing import Annotated,Literal
 import json
 
 
 app = FastAPI()
+
+class Patient(BaseModel):
+
+    id: Annotated[str,Field(...,description="ID of the patient", examples=['P001'])]
+    name:Annotated[str,Field(...,description="Name of the patient")]
+    city: Annotated[str,Field(...,description="City of the patient")]
+    age:Annotated[int,Field(...,gt=0,lt=120,description="Age of the patient")]
+    gender:Annotated[Literal["male","female","others"],Field(...,description="City of the patient")]
+    height:Annotated[float,Field(...,gt=0,description="Height of the patient in mtrs")]
+    weight:Annotated[float,Field(...,gt=0,description="Weight of the patient kgs")]
+
 
 
 
